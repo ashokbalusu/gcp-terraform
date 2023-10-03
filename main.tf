9a6f44a271c332eeb58ea511a0b86e2ac7792e3f
JUD8374 <jud8374@Ashoks-MacBook-Air.local>
2023-10-02 21:09:57 -0500

    adding terraform code for gcs creation

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "google_storage_bucket" "gcpttproject" {
+  name          = "ab-gcp-terraform-github-1"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
