4b4d889b18fd0b4fd7f37a16dd069739859a71fa
Ashok B <ab@h.com>
2023-10-08 15:08:12 -0500

    testing local file creation using terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
-resource "google_storage_bucket" "gcpttproject" {
-  name          = "ab-gcp-terraform-github-1"
-  location      = "US"
-  force_destroy = true
+# resource "google_storage_bucket" "gcpttproject" {
+#   name          = "ab-gcp-terraform-github-1"
+#   location      = "US"
+#   force_destroy = true
 
-  public_access_prevention = "enforced"
+#   public_access_prevention = "enforced"
+# }
+
+resource local_file testing {
+  sensitive_content = "testing local file creation"
+  filename             = "test.txt"
 }
-
