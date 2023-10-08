4e54206342184d167e717bc3d43f84a5c6f05c11
Ashok B <ab@h.com>
2023-10-08 15:19:14 -0500

    testing with random int and string generation using random terraform provider

--- main.tf
+++ main.tf
@@ -6,7 +6,24 @@
 #   public_access_prevention = "enforced"
 # }
 
-resource local_file testing {
-  content = "testing local file creation"
-  filename             = "test.txt"
+# resource local_file testing {
+#   content = "testing local file creation"
+#   filename             = "test.txt"
+# }
+
+resource random_integer rint {
+  min = 10
+  max = 30
+}
+
+resource random_string ran_string {
+  length  = 10
+}
+
+output rint_output {
+  value       = random_integer.rint.result
+}
+
+output ran_string_output{
+  value = random_string.ran_string.result
 }
